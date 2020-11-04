053b32d11ae97f48037b4aa649ba6494a9583ed1
cktufftask <61330012+cktufftask@users.noreply.github.com>
2020-11-05 01:08:01 +0530

    Add files via upload

---
+++ precache-manifest.856170adfe23a96bedcda335f6754bc8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5985ee74f1a471df5e42",
+    "url": "/shop/static/css/main.dd7c06da.chunk.css"
+  },
+  {
+    "revision": "5985ee74f1a471df5e42",
+    "url": "/shop/static/js/main.2e86bf65.chunk.js"
+  },
+  {
+    "revision": "162d8e4e66ca27cdb508",
+    "url": "/shop/static/js/runtime~main.265007fa.js"
+  },
+  {
+    "revision": "d9b004a11dbc633d4cdc",
+    "url": "/shop/static/css/2.f633775f.chunk.css"
+  },
+  {
+    "revision": "d9b004a11dbc633d4cdc",
+    "url": "/shop/static/js/2.09175cb9.chunk.js"
+  },
+  {
+    "revision": "fb32d79c48a30b6cc1e5a034026e5050",
+    "url": "/shop/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/shop/precache-manifest.da8f6906e2cd3c6f979f1eb7fc7b317d.js"
+  "/shop/precache-manifest.856170adfe23a96bedcda335f6754bc8.js"
 );
 
 workbox.clientsClaim();
