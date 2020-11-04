161f5d23b412988a0754c4844df0a75cf8943884
cktufftask <61330012+cktufftask@users.noreply.github.com>
2020-11-04 15:45:36 +0530

    file updated
    file updated

---
+++ precache-manifest.dda507979aadcc005147fc8df5467944.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "94c0479250e27fe12209",
+    "url": "/shop/static/css/main.dd7c06da.chunk.css"
+  },
+  {
+    "revision": "94c0479250e27fe12209",
+    "url": "/shop/static/js/main.38424117.chunk.js"
+  },
+  {
+    "revision": "162d8e4e66ca27cdb508",
+    "url": "/shop/static/js/runtime~main.265007fa.js"
+  },
+  {
+    "revision": "b91ce68dc24376a1d452",
+    "url": "/shop/static/css/2.f633775f.chunk.css"
+  },
+  {
+    "revision": "b91ce68dc24376a1d452",
+    "url": "/shop/static/js/2.b9c16aaf.chunk.js"
+  },
+  {
+    "revision": "efaf307f5b84a0b23fa639a609e2bca1",
+    "url": "/shop/index.html"
+  }
+];
